c0dbf98a0305b5647aa39198ff507669445c1053
Russell Hutson <russell.hutson@exertismicro-p.co.uk>
2015-10-23 10:55:23 +0100

    Add riot.js for templating and data binding. Support click through from list to job details

---
+++ js/job_details.tag.riot.js
@@ -0,0 +1,13 @@
+<job_details>
+    <div class="panel panel-default">
+        <div class="panel-heading {lead: opts.job.featured}">
+            <h3 class="panel-title ">{opts.job.job_title}</h3>
+        </div>
+        <div class="panel-body "><raw content="{ opts.job.job_description }"/></div>
+        <ul class="list-group">
+            <li class="list-group-item ">{opts.job.job_location}</li>
+            <li class="list-group-item ">{opts.job.job_reference}</li>
+        </ul>
+    </div>
+
+</job_details>
